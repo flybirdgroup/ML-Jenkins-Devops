76eab6a49e55a4389b350bed202885c5c5255d60
flybirdgroup <flybirdgroup@163.com>
2020-09-26 08:18:53 +0800

    updated docker run

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if docker ps | grep mldevops; then docker stop mldevops;
+if docker ps | grep mldevops; then docker stop mldevops;docker container rm mldevops;
 fi
 docker run -p 5000:5000 --name mldevops mldevops
